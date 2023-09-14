182203a6eddfb8dd5abd6a289235b2227b3b1156
song1560110 <song1560110@gmail.com>
2023-09-14 16:28:20 +0900

    파비콘

--- src/main/java/com/pinkward/bushgg/domain/article/Mapper/ArticleMapper.java
+++ src/main/java/com/pinkward/bushgg/domain/article/Mapper/ArticleMapper.java
@@ -8,18 +8,40 @@ import java.util.List;
 @Mapper
 public interface ArticleMapper {
 
-//	전체 게시글 목록 출력
+//	/** 신규 게시글 등록 */
+	public void create(ArticleDTO articleDTO);
+
+//	/** 전체 게시글 목록 반환 */
 	public List<ArticleDTO> findAll();
 
-	//	게시글 생성
-	public void create();
+//	/** 페이징 계산에 필요한 게시글 전체 갯수 반환 */
+	public int getCountAll();
 
-//	게시글 수정
-	public void updateArticle(String passwd);
+//	/** 페이징 계산(검색값 포함)에 필요한 게시글 전체 갯수 반환 */
+//    public int getCountAll(PageParams pageParams);
 
-//	게시글 삭제
-	public void delete(String passwd);
+//	/** 요청 페이지, 페이지당 보여지는 목록 갯수에 따른 목록 반환 */
+//	public List<ArticleDTO> findByAll(PageParams pageParams);
 
+//	/** 댓글, 대댓글 쓰기, 게시글 상세보기, 게시글 수정, 게시글 삭제 등 기능 추가*/
+
+//	/** 댓글 등록*/
+	public void createComment(ArticleDTO articleDTO);
+
+//	/** 대댓글 등록*/
+	public void commentByComment(ArticleDTO articleDTO);
+
+	public List<ArticleDTO> readComment(int groupNo);
+
+
+//	/** 댓글 삭제*/
+	public void removeComment(int articleId, String passwd);
+
+//	/** 댓글 수정*/
+	public void update(ArticleDTO articleDTO);
+
+//	/** 게시글 상세보기 */
+	public ArticleDTO detail(int articleId);
 
 	
 }
