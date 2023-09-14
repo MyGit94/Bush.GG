8a80079b301b52955c6517d411dd23de1e67eea1
lh01318 <lh01318@naver.com>
2023-09-14 16:51:45 +0900

    commit 09.14

--- src/main/java/com/pinkward/bushgg/domain/member/service/MemberServiceImpl.java
+++ src/main/java/com/pinkward/bushgg/domain/member/service/MemberServiceImpl.java
@@ -2,42 +2,44 @@ package com.pinkward.bushgg.domain.member.service;
 
 import java.util.List;
 
+import com.pinkward.bushgg.domain.member.mapper.MemberMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.pinkward.bushgg.domain.member.dto.MemberDTO;
-import com.pinkward.bushgg.domain.member.mapper.MemberMapper;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
-public class MemberServiceImpl implements MemberService{
-//	의존성주입... @RequiredArgsConstructor 이용해서 생성자 자동 생성
-	private final MemberMapper memberMapper;
+public class MemberServiceImpl implements MemberService {
+    //	의존성주입... @RequiredArgsConstructor 이용해서 생성자 자동 생성
+    private final MemberMapper memberMapper;
 
-	@Override
-	@Transactional
-	public void register(MemberDTO member) {
-		memberMapper.create(member);
-		
-	}
+    @Override
+    @Transactional
+    public void register(MemberDTO member) {
+        memberMapper.create(member);
 
-	@Override
-	public MemberDTO isMember(String loginid, String passwd) {
-		
-		return memberMapper.findByIdAndPasswd(loginid,passwd);
-	}
+    }
 
-	@Override
-	public List<MemberDTO> getMemberList() {
-		return memberMapper.findByAll();
-	}
+    @Override
+    public MemberDTO isMember(String loginid, String passwd) {
 
-	@Override
-	public MemberDTO getMember(String nickName) {
-		return memberMapper.findByNick(nickName);
-	}
-	
+        return memberMapper.findByIdAndPasswd(loginid, passwd);
+    }
+
+    @Override
+    public List<MemberDTO> getMemberList() {
+
+        return memberMapper.findByAll();
+    }
+
+    @Override
+    public MemberDTO getMember(String nickName) {
+
+        return memberMapper.findByNick(nickName);
+    }
+
 
 }
