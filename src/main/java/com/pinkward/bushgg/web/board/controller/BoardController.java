6f0c6b21a09853e48cd8d867e2fedd86c2504305
song1560110 <song1560110@gmail.com>
2023-09-15 17:40:15 +0900

    record 디자인변경 , index 티어 사이즈변경

--- src/main/java/com/pinkward/bushgg/web/board/controller/BoardController.java
+++ src/main/java/com/pinkward/bushgg/web/board/controller/BoardController.java
@@ -1,5 +1,7 @@
 package com.pinkward.bushgg.web.board.controller;
 
+import com.pinkward.bushgg.domain.article.DTO.ArticleDTO;
+import com.pinkward.bushgg.domain.article.Mapper.ArticleMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,20 +9,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/board")
 @Slf4j
+@RequiredArgsConstructor
 public class BoardController {
+
+	private final ArticleMapper articleMapper;
+	private final int ELEMENT_SIZE = 8;
+	private final int PAGE_SIZE = 5;
 	
 	@GetMapping
 	public String article(Model model) {
-		log.info("게시판 목록 요청");
+		List<ArticleDTO> list = articleMapper.findAll();
+		model.addAttribute("list" , list);
 		return "article/board";
 	}
 	
 	@GetMapping("/register")
 	public String register(Model model) {
-		log.info("게시글 목록 요청");
+
 		return "article/board-register";
 	}
 	
