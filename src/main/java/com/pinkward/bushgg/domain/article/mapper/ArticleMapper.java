2abbf36296fce848b61b695ac7ebdb040c309fc3
song1560110 <song1560110@gmail.com>
2023-09-18 14:12:21 +0900

    페이징 작업중 (게시글 리스트 안들어가짐 아직 ㅠㅠ) , 게시글 리스트 , 게시글 등록 , 게시글 디테일 추가됨

--- src/main/java/com/pinkward/bushgg/domain/article/mapper/ArticleMapper.java
+++ src/main/java/com/pinkward/bushgg/domain/article/mapper/ArticleMapper.java
@@ -1,6 +1,7 @@
 package com.pinkward.bushgg.domain.article.mapper;
 
 import com.pinkward.bushgg.domain.article.dto.ArticleDTO;
+import com.pinkward.bushgg.domain.common.web.PageParams;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -18,7 +19,7 @@ public interface ArticleMapper {
 	public int getCountAll();
 
 //	/** 페이징 계산(검색값 포함)에 필요한 게시글 전체 갯수 반환 */
-//    public int getCountAll(PageParams pageParams);
+    public int getCountAll(PageParams pageParams);
 
 //	/** 요청 페이지, 페이지당 보여지는 목록 갯수에 따른 목록 반환 */
 //	public List<ArticleDTO> findByAll(PageParams pageParams);
@@ -43,5 +44,7 @@ public interface ArticleMapper {
 //	/** 게시글 상세보기 */
 	public ArticleDTO detail(int articleId);
 
+//	게시글 상세보기 조회수 업
+	public void updateHitcount(ArticleDTO articleDTO);
 	
 }
--- src/test/java/com/pinkward/bushgg/ApplicationTests.java
+++ src/test/java/com/pinkward/bushgg/ApplicationTests.java
@@ -1,10 +1,13 @@
 package com.pinkward.bushgg;
 
+import com.pinkward.bushgg.domain.article.dto.ArticleDTO;
 import com.pinkward.bushgg.domain.article.mapper.ArticleMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 @Slf4j
@@ -15,9 +18,8 @@ class ApplicationTests {
 
 	@Test
 	void findAllTest() {
-//		List<ArticleDTO> list = articleMapper.findAll();
-//		log.info("받은 리스트" , list);
-
+		List<ArticleDTO> list = articleMapper.findAll();
+		log.info("받은 리스트" , list);
 	}
 
 }
